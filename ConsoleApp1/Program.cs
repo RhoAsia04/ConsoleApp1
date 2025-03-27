6fa453de8e64094aec6ba37c1383c264f7f0d3a1
Rhoda Asiamah <asiamaro@mail.uc.edu>
2025-03-27 11:59:45 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { }
+            public string MyNewProperty { get; set; }
+
+        // New method
+        public void MyNewMethod()
+        {
+            // Method implementation
+            Console.WriteLine("Hello, this is a method or property");
+        }
+    }
+}
